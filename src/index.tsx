ce1adc59fa4424952c9a2a740f36c33c81b3192e
SerStars <90714930+SerStars@users.noreply.github.com>
2023-12-24 22:27:02 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,21 @@
 import {Plugin, registerPlugin} from 'enmity/managers/plugins'
-import {React} from 'enmity/metro/common'
 import {create} from 'enmity/patcher'
 // @ts-ignore
 import manifest, {name as plugin_name} from '../manifest.json'
 import {getByProps} from "enmity/modules"
-import {get, set} from "enmity/api/settings"
+import {set} from "enmity/api/settings"
 
 // Make sure to disable "sync across clients"
 
-const canuse = getByProps("canUseClientThemes")
+const Themer = getByProps("updateBackgroundGradientPreset")
+const PermStat = getByProps("canUseClientThemes", {defaultExport: false});
+const {ExperimentStore} = getByProps('useAndTrackExposureToUserExperiment')
 const UserSettings = getByProps("setShouldSyncAppearanceSettings")
 
-const Patcher = create('FreeNitroTheme')
 
-const FreeNitroTheme: Plugin = {
+const Patcher = create('ClientThemes')
+
+const ClientThemes: Plugin = {
     ...manifest,
     onStart() {
         // disable theme sync
@@ -21,11 +23,50 @@ const FreeNitroTheme: Plugin = {
         Patcher.before(UserSettings, "setShouldSyncAppearanceSettings", (self, args, res) => {
             args[0] = false
         })
-        Patcher.instead("client-themes", getByProps("canUseClientThemes"), () => true)
+
+        // enable client theme experiment
+        Patcher.instead("canUseClientThemes", PermStat, () => true)
+        Patcher.after(ExperimentStore, 'getUserExperimentDescriptor', (self, [expName], res) => {
+            if (expName === "2023-02_client_themes_mobile" && res?.bucket) {
+                return {
+                    type: 'user',
+                    revision: 1,
+                    population: 0,
+                    bucket: 1,
+                    override: true
+                }
+            }
+        })
+
+
+        if (Object.isFrozen(PermStat.default)) {
+            PermStat.default = {...PermStat.default}
+        }
+        // make client theme available
+        Patcher.instead(PermStat.default, "canUseClientThemes", (_, args, __) => {
+            return true
+        })
+
+
+        // detect theme selection
+        Patcher.after(Themer, "updateMobilePendingThemeIndex", (_, args, __) => {
+            // it uses mobileThemesIndex, which is different from presetId
+            if (0 <= args[0] && args[0] <= 2) { // non nitro client theme
+                set(plugin_name, "theme", -1)  // at this time, update background gradient preset won't be called. so I need to reset the value I store manually
+            }
+            // console.log(`change: ${args[0]}`)
+        })
+
+        // detect theme application
+        Patcher.after(Themer, "updateBackgroundGradientPreset", (_, args, __) => {
+            // on change or on apply at startup (uses presetId)
+            // 8~19: dark / 0~7: light
+            set(plugin_name, "theme", args[0])
+            // console.log(`apply: ${args[0]}`)
+        })
     },
     onStop() {
         Patcher.unpatchAll()
     }
 }
-
-registerPlugin(FreeNitroTheme)
+registerPlugin(ClientThemes)
